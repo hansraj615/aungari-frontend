154b60b703f505bb6c1d888c3afa5622483b293d
sagar <sagar@einsicht-technologies.com>
2025-06-27 21:06:24 +0530

    gallery and fixes

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,10 @@
 // src/constants.js
 
-// export const API_BASE_URL = "https://api.adt.org.in/api";
-// export const IMAGE_BASE_URL = "https://api.adt.org.in/storage/";
+export const API_BASE_URL = "https://api.adt.org.in/api";
+export const IMAGE_BASE_URL = "https://api.adt.org.in/storage/";
 
-export const API_BASE_URL = "http://127.0.0.1:8001/api";
-export const IMAGE_BASE_URL = "http://127.0.0.1:8001/storage/";
+// export const API_BASE_URL = "http://127.0.0.1:8001/api";
+// export const IMAGE_BASE_URL = "http://127.0.0.1:8001/storage/";
 
 // Optional additional constants
 export const DEFAULT_VIDEO_RATIO = "16x9";
